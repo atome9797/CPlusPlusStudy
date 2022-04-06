1835bf368f95af12fe77421364414c840b7b2aee
atome9797 <65073631+atome9797@users.noreply.github.com>
2022-04-06 18:23:35 +0900

    20220406 commit #2
    2) 변수의 표기법

---
+++ 220406_HelloVariable/220406_HelloVariable/소스.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+	string name = "±è¿µÈÆ";
+	int age = 26;
+	int height = 176;
+	string blood_type = "A";
+	string mbti = "INTP";
+
+	string favorite_color = "black";
+	string job = "programer";
+	string language = "korean";
+	string tel = "010-5765-9269";
+	string local = "korea";
+
+	string mail = "hungame@naver.com";
+	string hobby = "read book";
+	string favorite_food = "stake";
+	string available_language = "japanese, korean";
+	string last_education = "high-school";
+	
+	string favorite_movie = "The Avengers";
+	string favorite_singer = "IU";
+	string dream = "Augmented Reality Developer";
+	string favorite_game = "League of Legends";
+	int favorite_number = 7;
+	
+
+}
